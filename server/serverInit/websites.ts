5223f862e7fbaee3869518715e1599babbd2e334
koudyt <info@digitalniweb.cz>
2022-11-20 17:24:55 +0100

    api routes refactored

--- server/serverInit/websites.ts
+++ server/serverInit/websites.ts
@@ -1,20 +1,22 @@
-/* import Publisher from "./../../custom/helpers/publisherService";
-import Subscriber from "./../../custom/helpers/subscriberService"; */
+import Publisher from "./../../custom/helpers/publisherService";
+import Subscriber from "./../../custom/helpers/subscriberService";
 import cron from "node-cron";
 import { billingModules } from "../../custom/helpers/billing";
 export default async function () {
-	/* let subscriber = Subscriber;
+	let subscriber = Subscriber;
 	console.log(await subscriber.subscribe("test-channel"));
 	let publisher = Publisher;
 	publisher.publish("test-channel", "test message");
+
 	subscriber.on("message", (channel, message) => {
-		console.log(`Received ${message} from ${channel}`);
+		console.log(`Received ${ message } from ${ channel }`);
 	});
-	console.log("websites serverInit"); */
+
+	console.log("websites serverInit");
 
 	// billingModules();
-	cron.schedule("0 2 * * *", () => {
+	/* cron.schedule("0 2 * * *", () => {
 		// 2 am every day
 		billingModules();
-	});
+	}); */
 }
