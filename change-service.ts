b713310a4e766dbf8b2deb09d47dcdbd85862038
koudyt <digitalniweb@gmail.com>
2022-12-18 18:51:16 +0100

    change-service.ts promises fix

--- change-service.ts
+++ change-service.ts
@@ -1,52 +1,61 @@
 import { argv, env, exit } from 'node:process';
 import dotenv from "dotenv";
-import { copyFile } from 'node:fs';
+import { copyFile } from 'node:fs/promises';
 
-class changeService {
+class ChangeService {
     newService: string | undefined;
     currentService: string | undefined;
     folder = 'service_configs/';
-    envVar= 'MICROSERVICE_NAME';
+    envVar = 'MICROSERVICE_NAME';
 
     constructor() {
         dotenv.config();
         let flags = argv.slice(2);
-        if(flags.length == 0)
+        if (flags.length == 0)
             throw `You have to set service name witch to switch to.`;
 
         this.newService = flags[0];
         this.currentService = env[this.envVar];
 
-        if(this.currentService === undefined)
+        if (this.currentService === undefined)
             throw `${this.envVar} in .env is undefined.`;
 
-        if(this.currentService == this.newService)
+        if (this.currentService == this.newService)
             throw `Current service name in .env is the same as service name you want to switch to.`;
-
-        this.copyFileToFolder('.env');
-        this.copyFileToFolder('package.json');
-        this.copyFileToFolder('package-lock.json');
-        
-        this.copyFileFromFolder('.env');
-        this.copyFileFromFolder('package.json');
-        this.copyFileFromFolder('package-lock.json');
     }
 
-    copyFileToFolder(file: string) {
+    async copyFiles() {
+        await this.copyFileToFolder('.env');
+        await this.copyFileToFolder('package.json');
+        await this.copyFileToFolder('package-lock.json');
+
+        await this.copyFileFromFolder('.env');
+        await this.copyFileFromFolder('package.json');
+        await this.copyFileFromFolder('package-lock.json');
+    }
+
+    async copyFileToFolder(file: string) {
         let newFileName = file + '_' + this.currentService
-        copyFile(file, this.folder + newFileName, 0, (err) => {
-            if(err) throw err; 
-            console.log(`${file} file copied to ${this.folder} as ${newFileName}.`);        
-        });
+        try {
+            await copyFile(file, this.folder + newFileName, 0);
+            console.log(`${file} file copied to ${this.folder} as ${newFileName}.`);
+        } catch (error) {
+            console.log(error);
+
+        }
     }
 
-    copyFileFromFolder(file: string) {
+    async copyFileFromFolder(file: string) {
         let newFileName = file + '_' + this.newService
-        copyFile(this.folder + newFileName, file, 0, (err) => {
-            if(err) throw err; 
-            console.log(`${newFileName} file copied from ${this.folder} as ${file}.`);        
-        });
+        try {
+            await copyFile(this.folder + newFileName, file, 0);
+            console.log(`${newFileName} file copied from ${this.folder} as ${file}.`);
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 }
 
-new changeService();
+let changeService = new ChangeService();
+await changeService.copyFiles();
