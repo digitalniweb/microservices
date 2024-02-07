159af96151009990b779222c31c77bfc040442b3
koudyt <digitalniweb@gmail.com>
2024-02-07 22:10:49 +0100

    get globalData model list fix

---
+++ custom/variables/globalData.ts
@@ -0,0 +1,7 @@
+import Role from "../../server/models/globalData/role.js";
+import Language from "../../server/models/globalData/language.js";
+
+export const globalDataModelsListMap = {
+	roles: Role,
+	languages: Language,
+};
