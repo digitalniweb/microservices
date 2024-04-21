2b7fcca78a5243e6e8f5e75616c5f1c11996c79e
koudyt <digitalniweb@gmail.com>
2024-04-21 18:51:47 +0200

    get language Ids

--- server/api/websites/current/index.ts
+++ server/api/websites/current/index.ts
@@ -3,5 +3,6 @@ const router = express.Router();
 import * as controller from "../../../controller/api/websites/current/modules.js";
 
 router.get("/modulesIds", controller.getModulesIds);
+router.get("/languagesIds", controller.getLanguagesIds);
 
 export default router;
--- server/controller/api/websites/current/modules.ts
+++ server/controller/api/websites/current/modules.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Op, col, fn } from "sequelize";
 import WebsiteModule from "../../../../models/websites/websiteModule.js";
+import WebsiteLanguageMutation from "../../../../models/websites/websiteLanguageMutation.js";
 export const getModulesIds = async function (
 	req: Request,
 	res: Response,
@@ -26,6 +27,33 @@ export const getModulesIds = async function (
 			error,
 			code: 500,
 			message: "Couldn't get website's modules ids",
+		});
+	}
+};
+export const getLanguagesIds = async function (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		let websiteId = req.query.websiteId as string;
+		let languagesIds = "languagesIds";
+		let idsStringInstance = await WebsiteLanguageMutation.findOne({
+			attributes: [[fn("GROUP_CONCAT", col("languageId")), languagesIds]],
+			where: {
+				[Op.and]: [{ WebsiteId: websiteId }],
+			},
+		});
+		let idsString = idsStringInstance?.get(languagesIds) as string;
+		let ids: number[] = [];
+		if (idsString) ids = idsString.split(",").map(Number);
+
+		return res.send(ids);
+	} catch (error: any) {
+		return next({
+			error,
+			code: 500,
+			message: "Couldn't get website's languages ids",
 		});
 	}
 };
