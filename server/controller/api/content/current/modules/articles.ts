0ae7c3581ca95c922fd9381b6275843da1b0ce6a
koudyt <digitalniweb@gmail.com>
2024-05-16 21:55:07 +0200

    content current  modules article get request

--- server/controller/api/content/current/modules/articles.ts
+++ server/controller/api/content/current/modules/articles.ts
@@ -1,12 +1,46 @@
 import { Request, Response, NextFunction } from "express";
 import db from "../../../../../models/index.js";
+import Article from "../../../../../models/content/article.js";
+import WidgetContent from "../../../../../models/content/widgetContent.js";
+import { getArticleQuery } from "../../../../../../digitalniweb-types/apps/communication/modules/articles.js";
+import { resourceIdsType } from "../../../../../../digitalniweb-types/apps/communication/index.js";
 export const getArticle = async function (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) {
 	try {
-		return res.send("articles");
+		let { resourceIds, url } = req.query as getArticleQuery;
+		if (typeof resourceIds === "string")
+			resourceIds = JSON.parse(resourceIds) as resourceIdsType;
+
+		let article = await db.transaction(async (transaction) => {
+			return await Article.findOne({
+				where: {
+					languageId: resourceIds.languageId,
+					websiteId: resourceIds.websiteId,
+					websitesMsId: resourceIds.websitesMsId,
+					url,
+				},
+				transaction,
+			});
+		});
+
+		if (!article) return res.send(null);
+
+		let widgetContents = await db.transaction(async (transaction) => {
+			return await WidgetContent.findAll({
+				where: {
+					moduleRecordId: article.id,
+					moduleId: resourceIds.moduleId,
+				},
+				transaction,
+			});
+		});
+		return res.send({
+			moduleInfo: widgetContents,
+			widgetContents,
+		});
 	} catch (error: any) {
 		return next({
 			error,
