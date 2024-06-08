093d6e9fea67c29774b65476ada80990140d7ff0
Mirec <mirectec@gmail.com>
2024-06-08 21:54:30 +0200

    turn off console.log redis

--- server/config/redisClient.ts
+++ server/config/redisClient.ts
@@ -1,12 +1,12 @@
 import IoRedis from "ioredis";
 import dotenv from "dotenv";
 
-// Načtení environmentálních proměnných
+// Load environmental variables
 dotenv.config();
 
 const redis = new IoRedis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
 });
-console.log(redis);
+//console.log(redis);
 export default redis;
