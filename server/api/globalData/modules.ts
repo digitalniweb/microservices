9db78086f493a2cb4611556322830a028178779b
koudyt <digitalniweb@gmail.com>
2024-07-10 21:00:55 +0200

    GlobalDataModelArray + Module array get

--- server/api/globalData/modules.ts
+++ server/api/globalData/modules.ts
@@ -4,5 +4,8 @@ import * as controller from "../../controller/api/globalData/modules.js";
 
 router.get("/list", controller.getModulesList);
 router.get("/listbyids", controller.getModulesByIds);
+router.get("/listbynames", controller.getModulesByNames);
+router.get("/listofidsbynames", controller.getModulesIdsByNames);
+router.get("/array", controller.getArray);
 
 export default router;
--- server/controller/api/globalData/modules.ts
+++ server/controller/api/globalData/modules.ts
@@ -2,9 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import db from "../../../models/index.js";
 
 import Module from "../../../models/globalData/module.js";
-import { getRequestGlobalDataModelList } from "../../../../digitalniweb-custom/helpers/getGlobalData.js";
+import {
+	getRequestGlobalDataModelArray,
+	getRequestGlobalDataModelList,
+} from "../../../../digitalniweb-custom/helpers/getGlobalData.js";
 import ModulePageLanguage from "../../../models/globalData/modulePageLanguage.js";
 import ModulePage from "../../../models/globalData/modulePage.js";
+import { Includeable, InferAttributes } from "sequelize";
 
 export const getModulesList = async function (
 	req: Request,
@@ -15,6 +19,102 @@ export const getModulesList = async function (
 		let data = await getRequestGlobalDataModelList(req, Module);
 
 		return res.send(data);
+	} catch (error) {
+		return next({
+			error,
+			code: 500,
+			message: "Couldn't get modules list.",
+		});
+	}
+};
+
+export const getArray = async function (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const responseArray = await getRequestGlobalDataModelArray(req, Module);
+		console.log(responseArray);
+
+		return res.send(responseArray);
+	} catch (error) {
+		return next({
+			error,
+			code: 500,
+			message: "Couldn't get modules list.",
+		});
+	}
+};
+
+export const getModulesIdsByNames = async function (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		let names = req.query.names as string[];
+		let modules = await db.transaction(async (transaction) => {
+			return await Module.findAll({
+				where: {
+					name: names,
+				},
+				transaction,
+			});
+		});
+		const modulesIds = modules.map((module) => module.id);
+		return res.send(modulesIds);
+	} catch (error) {
+		return next({
+			error,
+			code: 500,
+			message: "Couldn't get modules list.",
+		});
+	}
+};
+
+export const getModulesByNames = async function (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		let names = req.query.names as string[];
+		let attributes = req.query.attributes as string[] | undefined;
+
+		// in request this should be true or undefined
+		let include = req.query.include as
+			| boolean
+			| undefined
+			| Includeable
+			| Includeable[];
+		if (include === true) {
+			include = [
+				{
+					model: ModulePage,
+					include: [
+						{
+							model: ModulePageLanguage,
+						},
+					],
+				},
+			];
+		} else {
+			include = undefined;
+		}
+		if (!Array.isArray(names)) throw "Module names needs to be an array.";
+		let modules = await db.transaction(async (transaction) => {
+			return await Module.findAll({
+				where: {
+					name: names,
+				},
+				attributes,
+				transaction,
+				include,
+			});
+		});
+
+		return res.send(modules);
 	} catch (error) {
 		return next({
 			error,
