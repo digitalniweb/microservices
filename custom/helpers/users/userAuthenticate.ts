442f7fc0853511c5313145093d104b288dfa9ffb
koudyt <digitalniweb@gmail.com>
2024-07-14 20:20:33 +0200

    get user in auth with userModule instead of privilege

--- custom/helpers/users/userAuthenticate.ts
+++ custom/helpers/users/userAuthenticate.ts
@@ -1,19 +1,24 @@
 import { hashString } from "../../../digitalniweb-custom/functions/hashString.js";
 import User from "../../../server/models/users/user.js";
-import UserPrivilege from "../../../server/models/users/userPrivilege.js";
+import UserModule from "../../../server/models/users/userModule.js";
 import { log } from "../../../digitalniweb-custom/helpers/logger.js";
 import { commonError } from "../../../digitalniweb-types/customHelpers/logger.js";
 
+/**
+ * You need to decide in calling method what to do with all the information including password etc.
+ * Remove some of them before sending to user (as in api/controller/users -> authenticate) etc.
+ * @param login
+ * @param password
+ * @returns returns all data including **password** and **refreshSalt**!
+ */
 export async function userAuthenticate(login: string, password: string) {
-	// !!! this returns all data including password and refreshSalt !!!
-	// You need to decide in calling method what to do with these information. Remove some of them before sending to user (as in api/controller/users -> authenticate) etc.
 	try {
 		const user = await User.findOne({
 			where: { email: login, active: 1 },
 			paranoid: true,
 			include: [
 				{
-					model: UserPrivilege,
+					model: UserModule,
 				},
 			],
 		});
